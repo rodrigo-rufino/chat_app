c0bf40769af7027380ef06cb3f164d5494c10f63
rodrigo-rufino <rodrigo.srs.br@gmail.com>
2019-10-06 17:06:56 -0300

    [websocket] rendering messages with templates

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,8 +7,19 @@ const $messageFormButton = $messageForm.querySelector('button');
 
 const $sendLocationButton = document.querySelector('#send-location');
 
+const $messages = document.querySelector('#messages');
+
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+
+
 socket.on('message', (message) => {
   console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message
+  });
+  $messages.insertAdjacentHTML('beforeend', html);
 });
 
 socket.on('location', (location) => {
