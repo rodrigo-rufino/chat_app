81b26a8eb51e43c946339cc37e80c383dbf75072
rodrigo-rufino <rodrigo.srs.br@gmail.com>
2019-10-07 20:28:57 -0300

    color for user messages and autoscroll

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,17 +17,44 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
 
-
 const dateFormat = 'h:mm:ss a';
 
+
+const autoscroll = () => {
+  // new message element
+  const $newMessage = $messages.lastElementChild;
+  
+  // height of newMessage
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  // visible height
+  const visibleHeight = $messages.offsetHeight;
+
+  // height of messages container
+  const containerHeight = $messages.scrollHeight
+
+  // how far have i scrolled?
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+}
+
 socket.on('message', (message) => {
-  console.log(message);
+  const me = username === message.username ? true : false;
+
   const html = Mustache.render(messageTemplate, {
     username: message.username,
     message: message.text,
-    createdAt: moment(message.createdAt).format(dateFormat)
+    createdAt: moment(message.createdAt).format(dateFormat),
+    me
   });
+
   $messages.insertAdjacentHTML('beforeend', html);
+  autoscroll();
 });
 
 socket.on('location', (location) => {
@@ -38,6 +65,7 @@ socket.on('location', (location) => {
     createdAt: moment(location.createdAt).format(dateFormat)
   });
   $messages.insertAdjacentHTML('beforeend', html);
+  autoscroll();
 });
 
 
@@ -48,6 +76,8 @@ socket.on('roomData', ({ room, users }) => {
   });
   document.querySelector('#sidebar').innerHTML = html;
 })
+
+
 
 
 $messageForm.addEventListener('submit', (e) => {
@@ -85,6 +115,8 @@ $sendLocationButton.addEventListener('click', () => {
     });
   });
 });
+
+
 
 socket.emit('join', { username, room }, (error) => {
   if (error) {
